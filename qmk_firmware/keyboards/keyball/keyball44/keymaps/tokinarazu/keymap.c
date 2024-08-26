851205cbed726eb811bc44dfabeef4413410de3e
tokinarazu <tokinarazu@gmail.com>
2024-08-26 19:27:08 +0900

    keymap 修正

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tokinarazu/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tokinarazu/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [3] = LAYOUT_universal(
     _______  , _______  , _______  , _______  , _______  , _______ ,                                          S(KC_8)  , KC_7    , KC_8     , KC_9     , KC_SLSH  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______ ,                                          S(KC_MINS) , KC_4  , KC_5     , KC_6     , KC_MINUS , S(KC_SCLN) ,
+    _______  , _______  , _______  , _______  , _______  , _______ ,                                          S(KC_EQL), KC_4    , KC_5     , KC_6     , KC_MINUS , S(KC_SCLN) ,
     _______  , _______  , _______  , _______  , _______  , _______ ,                                          KC_0     , KC_1    , KC_2     , KC_3     , KC_DOT   , KC_NO    ,
                   QK_BOOT  , KBC_SAVE  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , _______  , KC_EQL
   ),
