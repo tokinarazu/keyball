ca316dd0ad520678a3fc01d5d5c58e1932291948
tokinarazu <tokinarazu@gmail.com>
2024-09-23 01:25:25 +0900

    sm_td 取消

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tokinarazu/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tokinarazu/rules.mk
@@ -8,10 +8,10 @@ VIA_ENABLE = yes
 # KEY_OVERRIDE_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
 CAPS_WORD_ENABLE = yes
-# EXTRAKEY_ENABLE = yes
-# DYNAMIC_MACRO_ENABLE = yes
-DEFERRED_EXEC_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+# DEFERRED_EXEC_ENABLE = yes
 
-# SRC += lib/keyball/keyball.c
+SRC += lib/keyball/keyball.c
 SRC += features/translate_ansi_to_jis.c
 SRC += features/select_word.c
